9d60455ee686c20f83b3a76eb94dad995355fdd7
McDavid Lubega <xenicmark@gmail.com>
2023-03-29 14:09:31 +0300

    implemented clients feature

---
+++ src/controllers/Clients.ts
@@ -0,0 +1,91 @@
+import { db } from '@utils/dbconnection';
+import { Request, Response } from 'express';
+import { Users } from '@prisma/client';
+
+const getAllClients = async (req: Request, res: Response): Promise<void> => {
+  const clients = await db.clients.findMany();
+  res.status(200).json(clients);
+};
+
+const getAClient = async (req: Request, res: Response): Promise<void> => {
+  const { id } = req.params;
+  const client = await db.clients.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!client) res.status(404).json({ message: 'Client not found' });
+
+  res.status(200).json(client);
+};
+
+const createAClient = async (req: Request, res: Response): Promise<void> => {
+  const { email, name, address, tel } = req.body;
+
+  const clientExists = await db.clients.findUnique({ where: { email } });
+
+  if (clientExists) {
+    res.status(400).json({ message: 'Client already exists' });
+    return;
+  }
+
+  const newClient = await db.clients.create({
+    data: {
+      email,
+      name,
+      address,
+      tel,
+    },
+  });
+
+  res.status(201).json(newClient);
+  return;
+};
+
+const updateClient = async (req: Request, res: Response): Promise<void> => {
+  const { id } = req.params;
+  const { email, name, address, tel } = req.body;
+
+  if (email) {
+    const emailExists = await db.clients.findFirst({ where: { email } });
+    if (emailExists) res.status(401).json({ message: 'Email already exists' });
+  }
+
+  const updatedClient = await db.clients.update({
+    where: {
+      id,
+    },
+    data: {
+      email,
+      name,
+      address,
+      tel,
+    },
+  });
+
+  res.status(200).json(updatedClient);
+  return;
+};
+
+const deleteClient = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const clientExists = await db.clients.findUnique({ where: { id } });
+  if (!clientExists) res.status(401).json({ message: 'Client not found' });
+  const deletedClient = await db.clients.delete({
+    where: {
+      id: id,
+    },
+  });
+
+  res.status(200).json([{ message: 'Client deleted' }, deletedClient]);
+  return;
+};
+
+export default {
+  getAllClients,
+  getAClient,
+  createAClient,
+  updateClient,
+  deleteClient,
+};
--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,184 +1,197 @@
-import { PrismaClient } from "@prisma/client";
-import { error } from "console";
-import argon from 'argon2'
+import { PrismaClient } from '@prisma/client';
+import { error } from 'console';
+import argon from 'argon2';
 
 const prisma = new PrismaClient();
-async function main(){
-    const mark = await prisma.users.upsert({
-        where:{email:'mark@email.com'},
-        update:{
-            
+async function main() {
+  const mark = await prisma.users.upsert({
+    where: { email: 'mark@email.com' },
+    update: {},
+    create: {
+      email: 'mark@email.com',
+      username: 'Mark David',
+      password: await argon.hash('secretpassword'),
+      role: 'ADMIN',
+      profile: {
+        create: {
+          bio: 'The chillest one of them all.',
         },
-        create:{
-            email:'mark@email.com',
-            username:'Mark David',
-            password: await argon.hash('secretpassword'),
-            profile:{
-                create:{
-                    bio:'The chillest one of them all.'
-                }
+      },
+      invoices: {
+        create: [
+          {
+            terms: 'Invoice due in 30 minutes',
+            items: {
+              create: [
+                {
+                  name: 'Item 1',
+                  description: 'Item 1 description',
+                  quantity: 2,
+                  unit_cost: 1000,
+                },
+                {
+                  name: 'Item 2',
+                  description: 'Item 2 description',
+                  quantity: 1,
+                  unit_cost: 200,
+                },
+                {
+                  name: 'Item 3',
+                  description: 'Item 3 description',
+                  quantity: 3,
+                  unit_cost: 3500,
+                },
+                {
+                  name: 'Item 4',
+                  description: 'Item 4 description',
+                  quantity: 4,
+                  unit_cost: 500,
+                },
+              ],
             },
-            invoices:{
-                create:[{
-                    title:'Invoice 1',
-                    terms:'Invoice due in 30 minutes',
-                    items:{
-                        create:[
-                            {
-                            description:'Item 1 description', 
-                            quantity:2,    
-                            unit_cost:1000   
-                        },
-                        {
-                            description:'Item 2 description', 
-                            quantity:1,    
-                            unit_cost:200   
-                        },
-                        {
-                            description:'Item 3 description', 
-                            quantity:3,    
-                            unit_cost:3500   
-                        },
-                        {
-                            description:'Item 4 description', 
-                            quantity:4,    
-                            unit_cost:500   
-                        }
-                        ],
-                    },
-                    client:{
-                        create:{
-                            email: 'info@shirtcat.com',
-                            name:'Shirtcat Carpentry', 
-                            address:'Kigundu Road Kitalanga, Kampala',
-                            tel:'256 789 826 6434',
-                        }
-                    },
-
-                }],
-            }
-        }
-    })
-    const timo = await prisma.users.upsert({
-        where:{email:'timo@email.com'},
-        update:{},
-        create:{
-            email:'timo@email.com',
-            username:'Timothy Ssali',
-            password: await argon.hash('secretpassword'),
-            profile:{
-                create:{
-                    bio:'The coolest one of them all.'
-                }
+            client: {
+              create: {
+                email: 'info@shirtcat.com',
+                name: 'Shirtcat Carpentry',
+                address: 'Kigundu Road Kitalanga, Kampala',
+                tel: '256 789 826 6434',
+              },
             },
-            invoices:{
-                create:[{
-                    title:'Invoice 2',
-                    terms:'Invoice due in 30 minutes',
-                    items:{
-                        create:[
-                            {
-                            description:'Item 1 description', 
-                            quantity:2,    
-                            unit_cost:1000   
-                        },
-                        {
-                            description:'Item 2 description', 
-                            quantity:1,    
-                            unit_cost:200   
-                        },
-                        {
-                            description:'Item 4 description', 
-                            quantity:4,    
-                            unit_cost:500   
-                        }
-                        ],
-                    },
-                    client:{
-                        create:{
-                            email: 'info@aic.com',
-                            name:'Agro Consortium Ltd', 
-                            address:'1 Vale, Naguru Hill, Kampala',
-                            tel:'256 789 8146 6443',
-                        }
-                    },
-
-                }],
-            }
-        }
-    })
-    const isaac = await prisma.users.upsert({
-        where:{email:'isaac@email.com'},
-        update:{},
-        create:{
-            email:'isaac@email.com',
-            username:'Isaac Mukasa',
-            password: await argon.hash('secretpassword'),
-            profile:{
-                create:{
-                    bio:'The wisest one of them all.'
-                }
+          },
+        ],
+      },
+    },
+  });
+  const timo = await prisma.users.upsert({
+    where: { email: 'timo@email.com' },
+    update: {},
+    create: {
+      email: 'timo@email.com',
+      username: 'Timothy Ssali',
+      password: await argon.hash('secretpassword'),
+      profile: {
+        create: {
+          bio: 'The coolest one of them all.',
+        },
+      },
+      invoices: {
+        create: [
+          {
+            terms: 'Invoice due in 30 minutes',
+            items: {
+              create: [
+                {
+                  name: 'Item 1',
+                  description: 'Item 1 description',
+                  quantity: 2,
+                  unit_cost: 1000,
+                },
+                {
+                  name: 'Item 2',
+                  description: 'Item 2 description',
+                  quantity: 1,
+                  unit_cost: 200,
+                },
+                {
+                  name: 'Item 4',
+                  description: 'Item 4 description',
+                  quantity: 4,
+                  unit_cost: 500,
+                },
+              ],
             },
-            invoices:{
-                create:[{
-                    title:'Invoice 3',
-                    terms:'Invoice due in 30 minutes',
-                    items:{
-                        create:[
-                            {
-                            description:'Item 1 description', 
-                            quantity:2,    
-                            unit_cost:1000   
-                        },
-                        {
-                            description:'Item 2 description', 
-                            quantity:1,    
-                            unit_cost:200   
-                        },
-                        {
-                            description:'Item 3 description', 
-                            quantity:4,    
-                            unit_cost:500   
-                        },
-                        {
-                            description:'Item 4 description', 
-                            quantity:2,    
-                            unit_cost:1500   
-                        },
-                        {
-                            description:'Item 5 description', 
-                            quantity:1,    
-                            unit_cost:5000   
-                        },
-                        {
-                            description:'Item 6 description', 
-                            quantity:2,    
-                            unit_cost:300   
-                        }
-                        ],
-                    },
-                    client:{
-                        create:{
-                            email: 'info@ctb.com',
-                            name:'Created To Build', 
-                            address:'1 Katalina, Kampala',
-                            tel:'256 789 8146 6443',
-                        }
-                    },
+            client: {
+              create: {
+                email: 'info@aic.com',
+                name: 'Agro Consortium Ltd',
+                address: '1 Vale, Naguru Hill, Kampala',
+                tel: '256 789 8146 6443',
+              },
+            },
+          },
+        ],
+      },
+    },
+  });
+  const isaac = await prisma.users.upsert({
+    where: { email: 'isaac@email.com' },
+    update: {},
+    create: {
+      email: 'isaac@email.com',
+      username: 'Isaac Mukasa',
+      password: await argon.hash('secretpassword'),
+      profile: {
+        create: {
+          bio: 'The wisest one of them all.',
+        },
+      },
+      invoices: {
+        create: [
+          {
+            terms: 'Invoice due in 30 minutes',
+            items: {
+              create: [
+                {
+                  name: 'Item 1',
+                  description: 'Item 1 description',
+                  quantity: 2,
+                  unit_cost: 1000,
+                },
+                {
+                  name: 'Item 2',
+                  description: 'Item 2 description',
+                  quantity: 1,
+                  unit_cost: 200,
+                },
+                {
+                  name: 'Item 3',
+                  description: 'Item 3 description',
+                  quantity: 4,
+                  unit_cost: 500,
+                },
+                {
+                  name: 'Item 4',
+                  description: 'Item 4 description',
+                  quantity: 2,
+                  unit_cost: 1500,
+                },
+                {
+                  name: 'Item 5',
+                  description: 'Item 5 description',
+                  quantity: 1,
+                  unit_cost: 5000,
+                },
+                {
+                  name: 'Item 6',
+                  description: 'Item 6 description',
+                  quantity: 2,
+                  unit_cost: 300,
+                },
+              ],
+            },
+            client: {
+              create: {
+                email: 'info@ctb.com',
+                name: 'Created To Build',
+                address: '1 Katalina, Kampala',
+                tel: '256 789 8146 6443',
+              },
+            },
+          },
+        ],
+      },
+    },
+  });
 
-                }],
-            }
-        }
-    })
-    
-
-    console.log({mark,})
+  console.log({ mark });
 }
 
-main().then(async ()=>{
-    await prisma.$disconnect()
-}).catch(async(e)=>{
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-})
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async e => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
